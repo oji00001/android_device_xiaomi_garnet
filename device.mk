cbdabd5182f36d24951edfe13413cbb2d5a395cc
Pranav Vashi <neobuddy89@gmail.com>
2022-06-25 02:41:51 +0530

    garnet: Enable blur
    * Changes QS transparency into blur effect.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- device.mk
+++ device.mk
@@ -144,6 +144,9 @@ PRODUCT_PACKAGES += \
     bootctrl.garnet \
     bootctrl.garnet.recovery
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.7.vendor \
