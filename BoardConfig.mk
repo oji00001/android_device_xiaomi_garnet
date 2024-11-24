170d0cf53a9249b1e95d73483d5a45ad342a8bc7
JYRRC <javixhtsweet@gmail.com>
2024-11-24 17:31:21 +0100

    garnet: set RELAX_USES_LIBRARY_CHECK to true
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,7 @@ BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
+RELAX_USES_LIBRARY_CHECK= true
 
 # Inherit from proprietary files for miuicamera
 -include vendor/xiaomi/garnet-miuicamera/products/board.mk
